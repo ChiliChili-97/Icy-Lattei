abf51fe508d6c58d460c43db5cef16bc7bf1e0e6
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-26 11:50:01 +0900

    :recycle: 사용하지 않는 class 삭제

--- src/main/java/com/sparta/project/icylattei/user/repository/TokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.project.icylattei.user.repository;
-
-import com.sparta.project.icylattei.user.entity.Token;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Token, String> {
-
-}
