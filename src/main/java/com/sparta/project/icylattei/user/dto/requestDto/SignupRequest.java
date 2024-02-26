83e9860022e3de4c6b2991424d0b8f27de43c3dc
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-26 15:56:07 +0900

    :recycle: 회원가입시, 이름과 비밀번호 제약 조건의 message 부분 수

--- src/main/java/com/sparta/project/icylattei/user/dto/requestDto/SignupRequest.java
+++ src/main/java/com/sparta/project/icylattei/user/dto/requestDto/SignupRequest.java
@@ -11,13 +11,11 @@ import lombok.Setter;
 public class SignupRequest {
 
     @NotBlank(message =  "아이디는 필수로 입력해야 합니다.")
-    @Size(min = 4, max = 10, message = "아이디는 4자 이상 10자 이하여야 합니다.")
-    @Pattern(regexp = "^[a-z0-9]+$")
+    @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "아이디는 영어 소문자와, 숫자(0~9)로 구성되어야 하며, 4자 이상 10자 이하여야 합니다")
     private String username;
 
     @NotBlank(message = "비밀번호는 필수로 입력해야 합니다.")
-    @Size(min = 8, max = 15, message = "비밀번호는 8자 이상 15자 이하여야 합니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9]+$")
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$", message = "비밀번호는 영어 대문자 및 소문자, 숫자(0~9)로 구성되어야 하며, 8자 이상 15자 이하여야 합니다.")
     private String password;
 
 }
