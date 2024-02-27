e9c2b18061a2cedaefd394dee1b30df45c56eb2c
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-27 18:36:02 +0900

    :sparkles: userservice 내의 sinup메서드 테스트 구현 (회원가입 성공 및 실패)

--- src/main/java/com/sparta/project/icylattei/user/entity/User.java
+++ src/main/java/com/sparta/project/icylattei/user/entity/User.java
@@ -1,6 +1,7 @@
 package com.sparta.project.icylattei.user.entity;
 
 import jakarta.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -14,7 +15,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class User {
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
